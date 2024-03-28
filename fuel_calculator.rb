f3379556e6667b2fdb27873dfcae6d7b8ecc7900
Vera Tretiak <verun.mak@gmail.com>
2024-03-28 09:49:38 +0100

    Create fuel_calculator.rb

---
+++ fuel_calculator.rb
@@ -0,0 +1,28 @@
+def calculate_fuel(mass, flight_route)
+    total_fuel = 0
+    flight_route.each_with_index do |(directive, gravity), index|
+        if directive == :launch
+            fuel = (mass * gravity * 0.042 - 33).floor
+        elsif directive == :land
+            fuel = (mass * gravity * 0.033 - 42).floor
+            if gravity == 1.62  # Check if landing on the Moon
+                additional_fuel = calculate_additional_fuel(mass, index, flight_route)
+                fuel += additional_fuel
+            end
+        end
+        total_fuel += fuel
+        mass += fuel
+    end
+    total_fuel
+end
+
+def calculate_additional_fuel(mass, current_index, flight_route)
+    additional_fuel = 0
+    (0...current_index).reverse_each do |i|
+        directive, gravity = flight_route[i]
+        if directive == :launch
+            additional_fuel += (mass * gravity * 0.042 - 33).floor
+        end
+    end
+    additional_fuel
+end
